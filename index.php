14d0df7fe1fbf3a3d6b7f6a23e13a1f86889f78f
AdrianRizkyAnanda <adrianrizky065@gmail.com>
2024-01-12 16:18:34 +0700

    first

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Adrian Sepatu</title>
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap"
+    rel="stylesheet">
+
+  <!-- Feather Icons -->
+  <script src="https://unpkg.com/feather-icons"></script>
+
+  <!-- My Style -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+  <!-- Navbar start -->
+  <nav class="navbar">
+    <a href="#" class="navbar-logo">Sepatu<span>Compass</span>.</a>
+
+    <div class="navbar-nav">
+      <a href="#home">Home</a>
+      <a href="#about">Tentang Kami</a>
+      <a href="#menu">Menu</a>
+      <a href="#products">Produk</a>
+      <a href="#contact">Kontak</a>
+    </div>
+
+    <div class="navbar-extra">
+      <a href="#" id="search-button"><i data-feather="search"></i></a>
+      <a href="#" id="shopping-cart-button"><i data-feather="shopping-cart"></i></a>
+      <a href="#" id="hamburger-menu"><i data-feather="menu"></i></a>
+    </div>
+
+    <!-- Search Form start -->
+    <div class="search-form">
+      <input type="search" id="search-box" placeholder="search here...">
+      <label for="search-box"><i data-feather="search"></i></label>
+    </div>
+    <!-- Search Form end -->
+    <!-- Shopping Cart start -->
+    <div class="shopping-cart">
+      <div class="cart-item">
+        <img src="img/sepatu.jpg" alt="Product 1">
+        <div class="item-detail">
+          <h3>Product 1</h3>
+          <div class="item-price">IDR 538K</div>
+        </div>
+        <i data-feather="trash-2" class="remove-item"></i>
+      </div>
+      <div class="cart-item">
+        <img src="img/sepatu.jpg" alt="Product 1">
+        <div class="item-detail">
+          <h3>Product 1</h3>
+          <div class="item-price">IDR 538K</div>
+        </div>
+        <i data-feather="trash-2" class="remove-item"></i>
+      </div>
+      <div class="cart-item">
+        <img src="img/sepatu.jpg" alt="Product 1">
+        <div class="item-detail">
+          <h3>Product 1</h3>
+          <div class="item-price">IDR 538K</div>
+        </div>
+        <i data-feather="trash-2" class="remove-item"></i>
+      </div>
+    </div>
+    <!-- Shopping Cart end -->
+
+  </nav>
+  <!-- Navbar end -->
+
+  <!-- Hero Section start -->
+  <section class="hero" id="home">
+    <div class="mask-container">
+      <main class="content">
+        <h1>Kualitas terjamin! <span>Segera dapatkan produk kami sekarang juga!</span></h1>
+        <p>Dapatkan cashback 10% hanya hari ini!Pesan sekarang!</p>
+      </main>
+    </div>
+  </section>
+  <!-- Hero Section end -->
+
+  <!-- About Section start -->
+  <section id="about" class="about">
+    <h2><span>Tentang</span> Kami</h2>
+
+    <div class="row">
+      <div class="about-img">
+        <img src="https://awsimages.detik.net.id/community/media/visual/2019/11/08/648f93fa-fc82-40be-8f01-45d761f8292d_34.jpeg?w=600&q=90" alt="Tentang Kami">
+      </div>
+      <div class="content">
+        <h3>Serahkan masalahmu kepada ahlinya.Kami siap melayanimu. </h3>
+        <p>Beli 2 gratis 1! Kapan lagi? Yuk,checkout sekarang!</p>
+        <p>Segera belanja di toko kami! Jangan sampai kehabisan!</p>
+      </div>
+    </div>
+  </section>
+  <!-- About Section end -->
+
+  <!-- Menu Section start -->
+  <section id="menu" class="menu">
+    <h2><span>Menu</span> Kami</h2>
+    <p>Kami menyediakan berbagai sepatu compass terbaik di Indonesia.
+    </p>
+
+    <div class="row">
+      <div class="menu-card">
+        <img src="https://images.tokopedia.net/img/cache/500-square/VqbcmM/2023/10/26/85999413-e19f-496d-b9b7-00b1204b3528.jpg" alt="Sepatu Compass" class="menu-card-img">
+        <h3 class="menu-card-title">- Sepatu Compass Retrograde Low Black White -</h3>
+        <p class="menu-card-price">IDR 550K</p>
+      </div>
+      <div class="menu-card">
+        <img src="https://lzd-img-global.slatic.net/g/p/e8f656d4847c1160cb231bdd31ed5a67.jpg_360x360q75.jpg_.webp" alt="Sepatu Compass" class="menu-card-img">
+        <h3 class="menu-card-title">- Sepatu Compass Retrograde low White Blue -</h3>
+        <p class="menu-card-price">IDR 550K</p>
+      </div>
+      <div class="menu-card">
+        <img src="https://lzd-img-global.slatic.net/g/p/5c7947b5e61997447736b0da13e351e6.jpg_360x360q75.jpg_.webp" alt="Sepatu Compass" class="menu-card-img">
+        <h3 class="menu-card-title">- Sepatu Compass Gazelle Black White -</h3>
+        <p class="menu-card-price">IDR 450K</p>
+      </div>
+      <div class="menu-card">
+        <img src="https://lzd-img-global.slatic.net/g/ff/kf/Saa2435a706a04740a56b54ba255fbc8bR.jpg_360x360q75.jpg_.webp" alt="Sepatu Compass" class="menu-card-img">
+        <h3 class="menu-card-title">- Sepatu Compass Gazelle White Blue -</h3>
+        <p class="menu-card-price">IDR 450K</p>
+      </div>
+      <div class="menu-card">
+        <img src="https://img.ws.mms.shopee.co.id/16f62365e30e691e82a89a6f100d2a07" alt="Sepatu Compass" class="menu-card-img">
+        <h3 class="menu-card-title">- Sepatu Compass Gazelle Low Cream -</h3>
+        <p class="menu-card-price">IDR 450K</p>
+      </div>
+      <div class="menu-card">
+        <img src="https://lzd-img-global.slatic.net/g/ff/kf/S8d04666717664729b403af551b90f46bs.jpg_360x360q75.jpg_.webp" alt="Sepatu Compass" class="menu-card-img">
+        <h3 class="menu-card-title">- Sepatu Compass Gazelle Low White Red -</h3>
+        <p class="menu-card-price">IDR 450k</p>
+      </div>
+    </div>
+  </section>
+  <!-- Menu Section end -->
+
+  <!-- Products Section start -->
+  <section class="products" id="products">
+    <h2><span>Signature Produk</span> Kami</h2>
+    <p>Sepatu Compass ori tersedia banyak varian warna dan ukuran.untuk pemesanan silakan hubungi 081215848229. </p>
+      <div class="product-card">
+        <div class="product-icons">
+          <a href="order/transaksi.php"><i data-feather="shopping-cart"></i></a>
+        </div>
+        <div class="product-image">
+          <img src="https://images.tokopedia.net/img/cache/700/VqbcmM/2022/6/1/139bec62-9a7d-463b-921b-16876c3456d6.png" alt="Product 1">
+        </div>
+        <div class="product-content">
+          <h3>Sepatu Compass Retrograde Low Double black</h3>
+          <div class="product-stars">
+            <i data-feather="star"></i>
+            <i data-feather="star"></i>
+            <i data-feather="star"></i>
+            <i data-feather="star"></i>
+            <i data-feather="star"></i>
+          </div>
+          <div class="product-price">IDR 538K <span>IDR 560K</span></div>
+        </div>
+      </div>
+      <div class="product-card">
+        <div class="product-icons">
+          <a href="order/transaksi.php"><i data-feather="shopping-cart"></i></a>
+        </div>
+        <div class="product-image">
+          <img src="https://images.tokopedia.net/img/cache/700/VqbcmM/2022/6/1/139bec62-9a7d-463b-921b-16876c3456d6.png" alt="Product 1">
+        </div>
+        <div class="product-content">
+          <h3>Sepatu Compass Retrograde Low Double black</h3>
+          <div class="product-stars">
+            <i data-feather="star"></i>
+            <i data-feather="star"></i>
+            <i data-feather="star"></i>
+            <i data-feather="star"></i>
+            <i data-feather="star"></i>
+          </div>
+          <div class="product-price">IDR 538K <span>IDR 560K</span></div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Products Section end -->
+
+  <!-- Contact Section start -->
+  <section id="contact" class="contact">
+    <h2><span>Kontak</span> Kami</h2>
+    <p>Terimakasih Telah berbelanja!
+    </p>
+
+    <div class="row">
+      <form action="">
+        <div class="input-group">
+          <i data-feather="user"></i>
+          <input type="text" placeholder="nama">
+        </div>
+        <div class="input-group">
+          <i data-feather="mail"></i>
+          <input type="text" placeholder="email">
+        </div>
+        <div class="input-group">
+          <i data-feather="phone"></i>
+          <input type="text" placeholder="no hp">
+        </div>
+        <button type="submit" class="btn"><a href="order/transaksi.php">kirim pesan</a></button>
+      </form>
+
+    </div>
+  </section>
+  <!-- Contact Section end -->
+
+  <!-- Footer start -->
+  <footer>
+    <div class="socials">
+      <a href="https://instagram.com/_adrianrzky?igshid=MXM0c3NhbHpxemV0Mg=="><i data-feather="instagram"></i></a>
+      <a href="https://www.facebook.com/adrian.rizky.7374"><i data-feather="facebook"></i></a>
+    </div>
+
+    <div class="links">
+      <a href="#home">Home</a>
+      <a href="#about">Tentang Kami</a>
+      <a href="#menu">Menu</a>
+      <a href="#contact">Kontak</a>
+    </div>
+
+    <div class="credit">
+      <p>Created by <a href="">Adrian Rizky Ananda</a>. | &copy; 2023.</p>
+    </div>
+  </footer>
+  <!-- Footer end -->
+
+  <!-- Modal Box Item Detail start -->
+  <div class="modal" id="item-detail-modal">
+    <div class="modal-container">
+      <a href="#" class="close-icon"><i data-feather="x"></i></a>
+      <div class="modal-content">
+        <img src="img/products/1.jpg" alt="Product 1">
+        <div class="product-content">
+          <h3>Product 1</h3>
+          <p>Di order yuuuk shoes men and women bagus bagus cocok banget nih buat pasangan.</p>
+          <div class="product-stars">
+            <i data-feather="star" class="star-full"></i>
+            <i data-feather="star" class="star-full"></i>
+            <i data-feather="star" class="star-full"></i>
+            <i data-feather="star" class="star-full"></i>
+            <i data-feather="star"></i>
+          </div>
+          <div class="product-price">IDR 538K <span>IDR 560K</span></div>
+          <a href="#"><i data-feather="shopping-cart"></i> <span>add to cart</span></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Modal Box Item Detail end -->
+
+  <!-- Feather Icons -->
+  <script>
+    feather.replace()
+  </script>
+
+  <!-- My Javascript -->
+  <script src="js/script.js"></script>
+</body>
+
+</html>
